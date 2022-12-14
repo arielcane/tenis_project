c29f11127aaa664271df6414c07d6cbab4a77a9b
arielcane <120589932+arielcane@users.noreply.github.com>
2022-12-14 15:14:13 -0300

    Update torneo.php

--- view/torneo/torneo.php
+++ view/torneo/torneo.php
@@ -3,8 +3,16 @@
 <div align="center"><img src="assets/images/torneo.jpg"/></div>
 
 <form id="frm-torneo" action="?c=torneo&a=comenzar_torneo" method="post" enctype="multipart/form-data" name="fcontacto">
+<input type="hidden" name="genero" value="M">
 <div class="text-center">
-        <button class="btn btn-success">Jugar!</button>
+        <button class="btn btn-primary">Jugar Masculino!</button>
+    </div>
+</form>
+<br>
+<form id="frm-torneo" action="?c=torneo&a=comenzar_torneo" method="post" enctype="multipart/form-data" name="fcontacto">
+<input type="hidden" name="genero" value="F">
+<div class="text-center">
+        <button class="btn btn-danger">Jugar Femenino!</button>
     </div>
 </form>
 
